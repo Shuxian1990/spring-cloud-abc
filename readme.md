06804815c23b756dfe57d57d6f6c5744dfc9e7d4
shuxian <shuxian@huize.com>
2018-04-08 20:13:20 +0800

    调整目录，config-git-server

---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+## 版本信息
+
+|应用|版本|
+|--|--|
+|Spring-Boot|1.5.8.RELEASE|
+|spring-cloud-dependencies|Edgware.SR3|
--- spring-cloud-config/spring-cloud-config-server/src/main/java/com/printfcoder/abc/springcloud/config/server/SpringCloudConfigServer.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/com/printfcoder/abc/springcloud/config/server/SpringCloudConfigServer.java
@@ -1,12 +1,14 @@
-package com.printfcoder.abc.springcloud.config;
+package com.printfcoder.abc.springcloud.config.server;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ActuatorMetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@ActuatorMetricWriter
 public class SpringCloudConfigServer {
 
     public static void main(String[] args) {
