2e14c220e364384c023b71800fec15d547931b59
shuxian <printfcoder@gmail.com>
2018-04-24 10:22:22 +0800

    part 1 完成，完善命名

--- spring-cloud-home-plan-account-center/src/main/java/com/printfcoder/abc/springcloud/accountcenter/service/AccountService.java
+++ spring-cloud-home-plan-account-center/src/main/java/com/printfcoder/abc/springcloud/accountcenter/service/AccountService.java
@@ -2,13 +2,13 @@ package com.printfcoder.abc.springcloud.accountcenter.service;
 
 import com.printfcoder.abc.springcloud.accountcenter.domain.Account;
 import com.printfcoder.abc.springcloud.accountcenter.repository.AccountMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
-    @Autowired
+    @Resource
     private AccountMapper accountMapper;
 
     public Account queryAccountByLoginName(String loginName) {
