56a6aee2a6002d72b7b9de909318a2516638b430
shuxian <printfcoder@gmail.com>
2018-04-26 11:59:04 +0800

    增加打印日志信息

--- spring-cloud-home-plan-account-center/src/main/java/com/printfcoder/abc/springcloud/accountcenter/controller/AccountController.java
+++ spring-cloud-home-plan-account-center/src/main/java/com/printfcoder/abc/springcloud/accountcenter/controller/AccountController.java
@@ -2,7 +2,10 @@ package com.printfcoder.abc.springcloud.accountcenter.controller;
 
 import com.printfcoder.abc.springcloud.accountcenter.domain.Account;
 import com.printfcoder.abc.springcloud.accountcenter.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("account")
 public class AccountController {
 
+    Logger logger = LoggerFactory.getLogger(AccountController.class);
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
     @Autowired
     private AccountService accountService;
 
     @GetMapping("get-account-by-loginname-and-pwd")
     public Account getAccountByLoginNameAndPwd(@RequestParam("loginName") String loginName, @RequestParam("pwd") String pwd) {
+
+        logger.info("[getAccountByLoginNameAndPwd] 收到请求，本服务端口：{}", serverPort);
+
         Account account = accountService.queryAccountByLoginName(loginName);
         // 先只进行简单对比，暂不处理加密等
         if (account != null && pwd.equals(account.getPwd())) {
