8f137a1a2b9b5619371a1c5c35a64a8fc140080c
shuxian <printfcoder@gmail.com>
2018-05-09 16:17:57 +0800

    basic-part4-cloud-config

---
+++ spring-cloud-config-server/src/main/java/com/printfcoder/abc/springcloud/config/server/SpringCloudConfigServer.java
@@ -0,0 +1,16 @@
+package com.printfcoder.abc.springcloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ActuatorMetricWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@ActuatorMetricWriter
+public class SpringCloudConfigServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServer.class, args);
+    }
+}
